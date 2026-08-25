dcab677de60eefa7a1a93f0eb39363c079b44d7d
Jeel Patel <pateljeel1199@gmail.com>
2026-08-25 12:11:57 +0530

    Fix JSX parsing error in features.tsx

--- src/components/landing/features.tsx
+++ src/components/landing/features.tsx
@@ -84,7 +84,7 @@ export function Features() {
                       {feature.description}
                     </p>
 
-               
+
                   </div>
                 </div>
               </div>
